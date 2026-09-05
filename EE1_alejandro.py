c40307048ffab61377e18add6cc6358848c939f1
AlexOF1205 <161501620+AlexOF1205@users.noreply.github.com>
2026-09-05 17:52:13 -0600

    Add files via upload

--- EE1_alejandro.py
+++ EE1_alejandro.py
@@ -44,11 +44,40 @@ ee_rastrigin = EE1mas1(
     dim = 2,
     lim_min = -5.12,
     lim_max = 5.12,
-    sigma = 0.5,
+    sigma = 0.05,
     generaciones = 2000,
     seed = 42
 )
 
+class Lanzamiento:
+    def __init__(self, g = 9.81, x_muro = 40, h_muro = 15, x_blanco = 80, theta_min = 10, theta_max = 80, v_min = 5, v_max = 50):
+        self.g = g
+        self.x_muro = x_muro
+        self.h_muro = h_muro
+        self.x_blanco = x_blanco
+        self.theta_min = theta_min
+        self.theta_max = theta_max
+        self.v_min = v_min
+        self.v_max = v_max
+
+    def altura_en(self, x_pos, theta, v):
+        theta_rad = np.deg2rad(theta)
+        vx = v*np.cos(theta_rad)
+        vy = v*np.sin(theta_rad)
+        t = x_pos/vx
+        y = vy*t - 0.5*self.g*t**2
+        return y
+    
+    def alcance(self, theta, v):
+        theta_rad = np.deg2rad(theta)
+        R = v**2 * np.sin(2*theta_rad)/self.g
+        return R
+
+
+
+
+
+# MAIN
 mejor_x, mejor_aptitud, historial = ee_rastrigin.ejecutar()
 print("Mejor x encontrado: ", mejor_x)
 print("Mejor aptitud:", mejor_aptitud) 
@@ -58,4 +87,4 @@ plt.xlabel("Generación")
 plt.ylabel("Aptitud (Rastrigin)")
 plt.title("Curva de convergencia - EE(1+1) en Rastrigin")
 plt.show()
-# IRONEDIT:1788648447:ux23ii012:f10efbbcb3c05342fcdded47a62e64cc46c472c06f00b23e6aa7e000394de9ea
+# IRONEDIT:1788652309:ux23ii012:c7e36d285414478935da4f2891dd33457e22e9b82a0bf2de50c652a5700ed12a
